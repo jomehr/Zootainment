0efe1fdd605c53172d390f46baeb5d3a45ddd918
Melanie270 <45720224+Melanie270@users.noreply.github.com>
2018-12-18 11:06:13 +0100

    Add files via upload

--- iot/canonAction.py
+++ iot/canonAction.py
@@ -7,21 +7,49 @@ config = {
   "storageBucket": "zootainment-41365.appspot.com"
 }
 
+left = False
+right = False
+
 firebase = pyrebase.initialize_app(config)
+
+def move_left():
+	print "left"
+	
+
+def move_right():
+	print "right"
+
 
 db = firebase.database()
 
-def move_left(x):
+def move_left_handler(x):
+	global left
+	if x["data"] == True:
+    		left = True
+		move_left()
+	elif x["data"] == False:
+		left = False
+
+def move_right_handler(x):
+	global right
     	if x["data"] == True:
-		print "left"
-def move_right(x):
-    	if x["data"] == True:
-		print "right"
-def shoot(x):
+		right = True
+		move_right()
+    	elif x["data"] == False:
+		right = False
+
+def shoot_handler(x):
     	if x["data"] == True:
 		print "shoot"
 		
-my_stream = db.child("/cannon_actions/movement_left").stream(move_left)
-my_stream = db.child("/cannon_actions/movement_right").stream(move_right)
-my_stream = db.child("/cannon_actions/shoot").stream(shoot)
+my_stream_left = db.child("/cannon_actions/movement_left").stream(move_left_handler)
+my_stream_right = db.child("/cannon_actions/movement_right").stream(move_right_handler)
+my_stream_shoot = db.child("/cannon_actions/shoot").stream(shoot_handler)
+
+while True:
+	if left:
+		move_left()
+	if right:
+		move_right()
+
 
